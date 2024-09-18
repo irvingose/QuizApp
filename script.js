95f6fb03f9c55d0ce94863ee7975fbef44b60b2d
Franco Mitrushi <franco.9930@gmail.com>
2024-09-18 16:27:19 +0200

    feat: modified script.js

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
-const answerButtonsElement = document.getElementById("answer-buttons"); // Check the ID here
-
+const answerButtonsElement = document.getElementById("answer-buttons");
 let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
-  setNextQuestion(); // Add parentheses here to call the function
+  setNextQuestion();
 });
 
 function startGame() {
